fd503fad710f280dab78f064bbfcac616a8862a4
amchampoux <amchampoux@mirego.com>
2023-04-07 15:47:43 -0400

    complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store 
+  has_many :employees
+end
+
+class Employee 
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Julie", last_name: "Lemieux", hourly_rate: 55)
+@store1.employees.create(first_name: "Pedro", last_name: "Santos", hourly_rate: 50)
+@store2.employees.create(first_name: "William", last_name: "Dupuis", hourly_rate: 60)
+@store2.employees.create(first_name: "Alexandre", last_name: "Jean", hourly_rate: 55)
+@store2.employees.create(first_name: "Annie", last_name: "Brocoli", hourly_rate: 50)
